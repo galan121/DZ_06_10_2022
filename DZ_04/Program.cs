efed4e953514ac918f39bc328c12bb588e53511b
Andrey <galan121@yandex.ru>
2022-10-07 00:34:29 +0300

    DZ_04

---
+++ DZ_04/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задача 8: показать  чётные числа от 1 до N.
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2 ; 1<=N; i+=2 ) 
+Console.Write(i + " ");
